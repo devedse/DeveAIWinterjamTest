8169b4d54629f11988f1feef9ce5883ebb45f994
Devedse <davydavidse@gmail.com>
2024-01-29 17:24:04 +0100

    Fix add function in AmazingNonAiCalculator

--- SuperCalculator/AmazingNonAiCalculator.cs
+++ SuperCalculator/AmazingNonAiCalculator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,7 @@ namespace SuperCalculator
     {
         public int Add(int a, int b)
         {
-            return a * b;
+            return a + b;
         }
 
         public int Substract(int a, int b)
